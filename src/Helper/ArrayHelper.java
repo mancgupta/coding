bb8c0434c802c726cf511f49609e05479c0f7b55
manchit.gupta <manchit.gupta@tradus.com>
2012-10-13 19:56:11 +0530

    prime function added in array helper

--- src/Helper/ArrayHelper.java
+++ src/Helper/ArrayHelper.java
@@ -23,4 +23,15 @@ public class ArrayHelper {
                 ret[i][j] = a[i].charAt(j);
         return ret;
     }
+
+	public static boolean isPrime(long n) {
+		if(n < 2) return false;
+		if(n == 2 || n == 3) return true;
+		if(n%2 == 0 || n%3 == 0) return false;
+		for(long i = 6L; i*i <= n; i += 6) {
+			if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+		}
+		return true;
+	}
+
 }
