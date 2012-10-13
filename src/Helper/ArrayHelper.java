9749eef5c6c3eb5d3d919c3b0c027b60aa3c365b
manchit.gupta <manchit.gupta@tradus.com>
2012-10-13 22:11:16 +0530

    sieve added in array helper

--- src/Helper/ArrayHelper.java
+++ src/Helper/ArrayHelper.java
@@ -1,5 +1,7 @@
 package Helper;
 
+import java.util.Arrays;
+
 public class ArrayHelper {
     public static boolean [][]toBooleanArray(String []a){
         int r = a.length;
@@ -34,4 +36,50 @@ public class ArrayHelper {
 		return true;
 	}
 
+	public static boolean [] sieve(int n){
+		boolean []sieve = new boolean[n+1];
+		Arrays.fill(sieve,true);
+		sieve[0] = false;
+		sieve[1] = false;
+		for(int i=2;i*i<=n;i++)
+			for(int j = i*i;j<=n;j += i)
+				sieve[j] = false;
+		return sieve;
+	}
+
+	public static boolean [] sieveOfAtkin(int limit) {
+		boolean[] sieve = new boolean[limit + 1];
+
+		sieve[0] = false;
+		sieve[1] = false;
+		sieve[2] = true;
+		sieve[3] = true;
+
+		for (int x = 1; x*x <= limit; x++) {
+			for (int y = 1; y*y <= limit; y++) {
+
+				int n = (4 * x * x) + (y * y);
+				if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
+					sieve[n] = !sieve[n];
+				}
+				n = (3 * x * x) + (y * y);
+				if (n <= limit && (n % 12 == 7)) {
+					sieve[n] = !sieve[n];
+				}
+				n = (3 * x * x) - (y * y);
+				if (x > y && n <= limit && (n % 12 == 11)) {
+					sieve[n] = !sieve[n];
+				}
+			}
+		}
+		for (int n = 5; n*n <= limit; n++) {
+			if (sieve[n]) {
+				int x = n * n;
+				for (int i = x; i <= limit; i += x) {
+					sieve[i] = false;
+				}
+			}
+		}
+		return sieve;
+	}
 }
